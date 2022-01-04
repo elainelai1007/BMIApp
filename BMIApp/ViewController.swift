5beceb6cf80dc399070f0257430d8345a476d00a
Elaine <elaine@musedp.com>
2022-01-04 11:04:31 +0800

    Initial Commit

---
+++ BMIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMIApp
+//
+//  Created by Elaine Lai on 2022/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
